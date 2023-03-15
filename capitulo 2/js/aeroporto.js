42317e7a6ea955af8336d0d29244a932289d54da
Maria Eduarda Benevenutti <115738167+eduardabenevenutti77@users.noreply.github.com>
2023-03-15 11:10:09 -0300

    js

---
+++ capitulo 2/js/aeroporto.js
@@ -0,0 +1,9 @@
+const frm = document.querySelector("form")
+
+frm.addEventListener("submit", (e) => {
+
+    const uso = Number(frm.inValor.value)
+    const time = Number(frm.inTempo.value)
+
+    const calculo = uso * 
+})
